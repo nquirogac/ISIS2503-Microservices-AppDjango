b6e6ebc099bc4bcf8e2ee1114e7b4f54348d66cb
nquirogac <nquirogac@unal.edu.co>
2024-05-18 12:13:10 -0500

    fix

--- measurements/measurements/views.py
+++ measurements/measurements/views.py
@@ -51,7 +51,7 @@ def MeasurementsCreate(request):
         data_json = json.loads(data)
         measurement_list = []
         for measurement in data_json:
-                    if check_variable(measurement) == True and check_place(measurement) == True:
+                    if check_variable(measurement) == True:
                         db_measurement = Measurement()
                         db_measurement.variable = measurement['variable']
                         db_measurement.value = measurement['value']
